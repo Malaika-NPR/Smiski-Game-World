084c3db7aa4465d1e849f50f534ca83cc1294700
Crystal Hernandez <crystalhernandez2022@gmail.com>
2024-04-17 22:27:29 -0700

    <current room update>

---
+++ proj3/src/core/Rooms.java
@@ -0,0 +1,38 @@
+package core;
+
+import tileengine.Tileset;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Rooms {
+    private static final int MINDIMENSION = 3;
+    private static final int MAXDIMENSION = 12;
+
+    public Rooms(int width, int heigth) {
+    }
+
+    public List<Integer> randomRoomDimensions() {
+        List<Integer> dimensions = new ArrayList<>();
+        int roomWidth = randomDimension(MINDIMENSION, MAXDIMENSION);
+        int roomHeight = randomDimension(MINDIMENSION, MAXDIMENSION);
+        dimensions.add(roomWidth, roomHeight);
+        return dimensions;
+    }
+
+    public List<Integer> randomRoomCoordinates() {}
+
+    public boolean roomOverlapChecker() {
+        //take in a potential rooms coordinates
+        //check the surrounding 1 tile neighbors to make sure there isn't a live wall next to it
+        //if it doesnt overlap then returns false
+        //if it does overlap that it generates another random roomo until we get false
+    }
+
+    //returns a random width within bounds
+    public int randomDimension(int minDimension, int maxDimension) {
+        Random random = new Random();
+        return random.nextInt(maxDimension - minDimension) + minDimension;
+    }
+}
