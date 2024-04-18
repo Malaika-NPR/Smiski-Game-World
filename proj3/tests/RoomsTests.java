f6c12e8bcf9ece42dc24307ddf7b104cb1e5d1c2
Crystal Hernandez <crystalhernandez2022@gmail.com>
2024-04-18 12:44:28 -0700

    <random room population>

---
+++ proj3/tests/RoomsTests.java
@@ -0,0 +1,43 @@
+import core.Rooms;
+import org.junit.jupiter.api.Test;
+import tileengine.TETile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+import static core.Rooms.*;
+
+public class RoomsTests {
+    private static final int BOARDWIDTH = 80;
+    private static final int BOARDHEIGHT = 35;
+    Rooms rooms = new Rooms();
+    TETile[][] world = new TETile[BOARDWIDTH][BOARDHEIGHT];
+
+    @Test
+    public void randomRoomDimensionTest() {
+        rooms.randomRoomDimensions();
+        System.out.println(DIMENSIONS);
+    }
+
+    @Test
+    public void randomRoomCoordiantesTest() {
+        rooms.randomRoomCoordinates();
+        System.out.println(COORDINATES);
+    }
+
+    @Test
+    public void roomOverlapCheckerTest() {
+        ArrayList<List<Integer>> dimensions = new ArrayList<>();
+        ArrayList<List<Integer>> coordinates = new ArrayList<>();
+
+        boolean check = rooms.roomOverlapChecker(world);
+
+        System.out.println(check);
+    }
+
+    @Test
+    public void placeRandomRoomsTest() {
+        placeRandomRooms(world);
+    }
+}
