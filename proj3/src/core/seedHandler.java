befa2132f4818adefc525804e3422bcc23145f03
Crystal Hernandez <crystalhernandez2022@gmail.com>
2024-04-18 21:54:07 -0700

    <Room HashMap Coordinates>

--- proj3/src/core/seedHandler.java
+++ proj3/src/core/seedHandler.java
@@ -1,4 +1,8 @@
 package core;
 
 public class seedHandler {
+    //more random
+    //parse into the seed that's a string
+    //make it into a long int
+    //use when using Random random = new Random()
 }
